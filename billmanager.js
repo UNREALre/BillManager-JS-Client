5e66230e1a685f9ec444405acc36fafb12a9dae3
Alexander Podrabinovich <avpmanager@gmail.com>
2018-01-04 19:16:49 +0400

    - QoL апдейты; - Добавлен функционал: получение полной информации по всем продуктам пользователя; дата и сумма ближайшего платежа; вся информация по id проудктов в биллинге; смена пароля пользователя; пополнение баланса; покупка продукта с аддонами.

--- billmanager.js
+++ billmanager.js
@@ -2,15 +2,27 @@ var billManager = (function(){
 
 	var ajaxConnectionError = "Error during request to BILLmanager!";
 
+	function compare(a,b) {
+		var date1 = new Date(a.expiredate), date2 = new Date(b.expiredate);
+		if (date1 < date2)
+			return -1;
+
+		if (date1 > date2)
+			return 1;
+
+		return 0;
+	}
+
 	return {
 		options: {
 			bmURL: "",
 			errorContainerID: "",
 			sessionExpire: 0,
+			failToProlongSession: function(){},
 		},
 
 		//Authorize user in BM and saving auth data and bm session id within session storage
-		authorizeUser: function(login, password) {
+		authorizeUser: function(login, password, callback) {
 			var $this = this;
 			this.sendRequest({
 		    	"func": "auth",
@@ -19,14 +31,20 @@ var billManager = (function(){
 		        "out": "json"
 			}).done(function(response){
 				if (response.doc.error) {
-					$("#"+$this.options.errorContainerID).html(response.doc.error.msg.$);
+					callback({
+						error: response.doc.error.msg.$
+					});
 				} else {
 					sessionStorage.setItem("bm_authinfo", login+":"+password);
 					sessionStorage.setItem("bm_session_id", response.doc.auth.$id);
 					sessionStorage.setItem("bm_session_created", Date.now());
+
+					callback();
 				}
 			}).fail(function(response){
-				$("#"+$this.options.errorContainerID).html(ajaxConnectionError);
+				callback({
+					error: ajaxConnectionError
+				});
 			});
 		},
 
@@ -49,12 +67,48 @@ var billManager = (function(){
 	            "conditions_agree" : "on",
 			}).done(function(response){
 				if (response.doc.error) {
-					$("#"+$this.options.errorContainerID).html(response.doc.error.msg.$);
+					callback({
+						error: response.doc.error.msg.$
+					});
 				} else {
 					callback(response.doc["user.id"].$);
 				}
 			}).fail(function(response){
-				$("#"+$this.options.errorContainerID).html(ajaxConnectionError);
+				callback({
+					error: ajaxConnectionError
+				});
+			});
+		},
+
+		//
+		changePassword: function(user_id, old_pass, new_pass, callback) {
+			if (!this.checkSession()) {
+				this.options.failToProlongSession()
+				return false;
+			}
+
+			var $this = this;
+			this.sendRequest({
+		    	"func": "usrparam",
+		        "old_passwd": old_pass,
+		        "passwd": new_pass,
+		        "clicked_button": "ok",
+		        "elid": user_id,
+		        "auth": sessionStorage.getItem("bm_session_id"),
+		        "sok": "ok",
+		        "out": "json"
+			}).done(function(response){
+				if (response.doc.error) {
+					callback({
+						error: response.doc.error.msg.$
+					});
+				} else {
+					callback();
+				}
+			}).fail(function(response){
+				callback({
+					error: ajaxConnectionError
+				});
 			});
 		},
 
@@ -63,16 +117,25 @@ var billManager = (function(){
 			var expiration_time = +sessionStorage.getItem("bm_session_created") + this.options.sessionExpire * 60000;
 			var cur_time = Date.now();
 			var is_session_valid = (expiration_time - cur_time > 0 ? true : false);
-			var auth_info = sessionStorage.getItem("bm_authinfo").split(":");
+			if (sessionStorage.getItem("bm_authinfo")) {
+				var auth_info = sessionStorage.getItem("bm_authinfo").split(":");
 
-			if (!is_session_valid) {
-				this.authorizeUser(auth_info[0], auth_info[1]);
+				if (!is_session_valid) {
+					this.authorizeUser(auth_info[0], auth_info[1], function(){});
+				}
+
+				return true;
+			} else {
+				return false;
 			}
 		},
 
 		//Getting basic information about user from BM
 		getUserInfo: function(callback) {
-			this.checkSession();
+			if (!this.checkSession()) {
+				this.options.failToProlongSession()
+				return false;
+			}
 
 			var $this = this;
 			this.sendRequest({
@@ -81,7 +144,9 @@ var billManager = (function(){
 		        "out": "json"
 			}).done(function(response){
 				if (response.doc.error) {
-					$("#"+$this.options.errorContainerID).html(response.doc.error.msg.$);
+					callback({
+						error: response.doc.error.msg.$
+					});
 				} else {
 					callback({
 						"id": response.doc.user_id.$,
@@ -92,13 +157,18 @@ var billManager = (function(){
 					});
 				}
 			}).fail(function(response){
-				$("#"+$this.options.errorContainerID).html(ajaxConnectionError);
+				callback({
+					error: ajaxConnectionError
+				});
 			});
 		},
 
 		//Getting user Balance from BM
 		getUserBalance: function(callback) {
-			this.checkSession();
+			if (!this.checkSession()) {
+				this.options.failToProlongSession()
+				return false;
+			}
 
 			var $this = this;
 			this.sendRequest({
@@ -107,7 +177,9 @@ var billManager = (function(){
 		        "out": "json"
 			}).done(function(response){
 				if (response.doc.error) {
-					$("#"+$this.options.errorContainerID).html(response.doc.error.msg.$);
+					callback({
+						error: response.doc.error.msg.$
+					});
 				} else {
 					var clean_balance = response.doc.balance.$.split(" ");
 					callback({
@@ -116,27 +188,309 @@ var billManager = (function(){
 					});
 				}
 			}).fail(function(response){
-				$("#"+$this.options.errorContainerID).html(ajaxConnectionError);
+				callback({
+					error: ajaxConnectionError
+				});
 			});
 		},
 
-		//Get info about all user products with bm_product_code (for example: soft)
-		getUserProducts: function(bm_product_code, callback) {
-			this.checkSession();
+		//Get info about all user products with bm_product_code (for example: soft).
+		getUserProducts: function(bm_product_code, add_props, addons, callback) {
+			if (!this.checkSession()) {
+				this.options.failToProlongSession()
+				return false;
+			}
 
 			var $this = this;
+			var products = [];
+			var created_date;
+			var expire_date;
+			var cur_product;
+			var addon_info = [];
 			this.sendRequest({
 		    	"func": bm_product_code,
 		        "auth": sessionStorage.getItem("bm_session_id"),
 		        "out": "json"
 			}).done(function(response){
 				if (response.doc.error) {
-					$("#"+$this.options.errorContainerID).html(response.doc.error.msg.$);
+					callback({
+						error: response.doc.error.msg.$
+					});
 				} else {
-					console.log(response);
+
+					if (response.doc.elem) {
+
+						var promises = [];
+
+						response.doc.elem.forEach(function(product){
+
+							created_date = product.createdate.$.split("-");
+							created_date = created_date[1]+"."+created_date[2]+"."+created_date[0];
+
+							expire_date = product.expiredate.$.split("-");
+							expire_date = expire_date[1]+"."+expire_date[2]+"."+expire_date[0];
+
+							cur_product = {
+			                    id: product.id.$,
+			                    name: product.name.$,
+			                    price: product.cost.$,
+			                    period: product.period.$,
+			                    created: created_date,
+			                    expiredate: expire_date,
+			                    status: product.status.$,//5 - processing; 2 - active; 1 - just ordered
+			                    item_status: product.item_status.$orig,
+							};
+
+							if (add_props.length) {
+								for (var i=0; i<add_props.length; i++) {
+									if (product.hasOwnProperty(add_props[i])) {
+										cur_product[add_props[i]] = product[add_props[i]].$;
+									}
+								}
+							}
+
+							if (addons.length) {
+
+								//We have to make array of promises to gather info about service addons
+								promises.push($this.sendRequest({
+							    	"func": bm_product_code+".edit",
+							    	"elid": product.id.$,
+							        "auth": sessionStorage.getItem("bm_session_id"),
+							        "out": "json",
+								}).done(function(data) {
+									
+									for (var i=0; i<addons.length; i++) {
+										if (data.doc.hasOwnProperty(addons[i])) {
+											addon_info.push({
+												product_id: product.id.$,
+												addon_id: addons[i],
+												value: data.doc[addons[i]].$,
+												addon_name: data.doc.messages.msg[addons[i]]
+											});
+											
+										}
+									}
+									
+								}));
+
+							}
+
+							products.push(cur_product);
+
+						});
+
+						if (addons.length) {
+							var promise = $.when.apply($, promises);
+							var tmp_addons;
+							promise.done(function(data) {//When all ajax queries to retive addon info done, assign addons to it products
+								for (var i=0; i<products.length; i++) {
+									tmp_addons = [];
+									for (var j=0; j<addon_info.length; j++) {
+										if (products[i].id == addon_info[j].product_id) {
+											tmp_addons.push(addon_info[j]);
+										}
+									}
+
+									products[i].addon_info = tmp_addons;
+								}
+
+								callback(products);
+							});
+						} else {
+							callback(products);
+						}
+
+					} else {
+						callback(products);
+					}
+					
 				}
 			}).fail(function(response){
-				$("#"+$this.options.errorContainerID).html(ajaxConnectionError);
+				callback({
+					error: ajaxConnectionError
+				});
+			});
+		},
+
+		//Get closest payment that user have to do
+		getNextPaymentInfo: function(bm_product_code, callback) {
+			this.getUserProducts(bm_product_code, [], [], function(products) {
+				var active_products = [];
+				var price = 0;
+				var closest_date;
+				var clean_price;
+
+				if (products.length) {
+					products.forEach(function(product){
+						if (product.status != 1) {
+							active_products.push(product);
+						}
+					});
+					active_products.sort(compare);
+
+					closest_date = active_products[0].expiredate;
+					active_products.forEach(function(product){//if there are several products with same expiration date - get sum of them
+						if (product.expiredate == closest_date) {
+							clean_price = product.price.split(" ");
+							price += +clean_price[0];
+						} else {
+							return false;
+						}
+					});
+
+				}
+
+				callback({
+					date: closest_date,
+					price: price
+				});
+			});
+		},
+
+		//Return all product (product IDs in products arg) data from BM
+		getProductsInfo: function(products, callback) {
+			if (!this.checkSession()) {
+				this.options.failToProlongSession()
+				return false;
+			}
+
+			var $this = this;
+			var good_products = [];
+			this.sendRequest({
+		    	"func": "pricelist.export",
+		        "auth": sessionStorage.getItem("bm_session_id"),
+		        "out": "json"
+			}).done(function(response){
+				if (response.doc.error) {
+					callback({
+						error: response.doc.error.msg.$
+					});
+				} else {
+					
+					products.forEach(function(id) {
+						for (var i=0; i<response.doc.pricelist.length; i++) {
+							if (id == response.doc.pricelist[i].id.$) {
+								good_products.push(response.doc.pricelist[i]);
+							}
+						}
+					});
+
+					callback(good_products);
+
+				}
+			}).fail(function(response){
+				callback({
+					error: ajaxConnectionError
+				});
+			});
+		},
+
+		//Adding funds to user balance
+		addFundsToBalance: function(amount, currency_iso, currency_bm_id, pay_method_id, callback) {
+			if (!this.checkSession()) {
+				this.options.failToProlongSession()
+				return false;
+			}
+
+			var $this = this;
+			this.sendRequest({
+		    	"func": "payment.add.pay",
+		        "auth": sessionStorage.getItem("bm_session_id"),
+				"amount": amount,
+				"amount_currency": amount + " " + currency_iso,
+				"clicked_button": "finish",
+				"newwindow": "extform",
+				"payment_currency": currency_bm_id,
+				"paymethod": pay_method_id,
+	            "randomnumber": Math.random().toString(36).substr(2, 12),
+	            "u": "n",
+	            "sok": "ok",
+	            "out": "json"
+			}).done(function(response){
+				if (response.doc.error) {
+					callback({
+						error: response.doc.error.msg.$
+					});
+				} else {
+					
+					var redirect_url = response.doc.ok.$, final_url = $this.options.bmURL.replace("/billmgr", "") + redirect_url + "&auth="+sessionStorage.getItem("bm_session_id"), payment_id = response.doc.payment_id.$;
+
+					callback(final_url);
+				}
+			}).fail(function(response){
+				callback({
+					error: ajaxConnectionError
+				});
+			});
+		},
+
+		//Purchase product for user
+		buyProduct: function(purchaseParams, callback) {
+			if (!this.checkSession()) {
+				this.options.failToProlongSession()
+				return false;
+			}
+
+			var $this = this;
+
+			//Adding to cart
+			var cart_params = {
+	            "func": purchaseParams.product_code+".order.param",
+	            "pricelist": purchaseParams.product_id,
+	            "period": purchaseParams.period,
+	            "auth": sessionStorage.getItem("bm_session_id"),
+	            "project": purchaseParams.project,
+	            "sok": "ok",
+	            "out": "json",
+			};
+			if (purchaseParams.addons.length) {
+				var tmp;
+				purchaseParams.addons.forEach(function(item){
+					tmp = item.split("=");
+					cart_params[tmp[0]] = tmp[1];
+				});
+			}
+
+			this.sendRequest(cart_params).done(function(response){
+				if (response.doc.error) {
+					callback({
+						error: response.doc.error.msg.$
+					});
+				} else {
+					var billorder_id = response.doc["billorder.id"].$;
+
+					$this.sendRequest({
+			            "func": "payment.add.pay",
+			            "auth": sessionStorage.getItem("bm_session_id"),
+						"amount": purchaseParams.amount,
+						"amount_currency": purchaseParams.amount + " " + purchaseParams.currency_iso,
+						"billorder": billorder_id,
+						"clicked_button": "finish",
+						"newwindow": "extform",
+						"payment_currency": purchaseParams.currency_bm_id,
+						"paymethod": purchaseParams.paymethod,
+			            "randomnumber": Math.random().toString(36).substr(2, 12),
+			            "u": "n",
+			            "sok": "ok",
+			            "out": "json",
+					}).done(function(bill_response){
+						if (response.doc.error) {
+							callback({
+								error: bill_response.doc.error.msg.$
+							});
+						} else {
+							
+							var redirect_url = bill_response.doc.ok.$, final_url = $this.options.bmURL.replace("/billmgr", "") + redirect_url + "&auth="+sessionStorage.getItem("bm_session_id");
+
+							callback(final_url);
+						}
+					});
+
+				}
+			}).fail(function(response){
+				callback({
+					error: ajaxConnectionError
+				});
 			});
 		},
 
--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 //Инициализируем
 billManager.options = {
 	bmURL: "https://185.14.30.1:1500/billmgr",
-	errorContainerID: "error",
 	sessionExpire: 30,
+	failToProlongSession: function() {
+		//будет вызвана, если сессия биллинга будет просрочена, а продлить ее не получится. тут можно описать те или иные действия
+	}
 }
 
 /*
@@ -23,10 +25,15 @@ billManager.registerUser(
 );
 */
 
+//Для всех методов общепринятый последний аргумент - callback функция. Если были какие-то ошибки во время выполнения запросов, то объект аргумента будет содержать св-во error с расшифровкой ошибки
+
 /*
 //Если данные верные, пользователь будет авторизован в системе, в Локальном хранилище браузера будет сохранен ID сессии биллинга
-billManager.authorizeUser("avpmanager99@gmail.com", "Alexander321");
+billManager.authorizeUser("avpmanager@gmail.com", "Alexander123", function(data){
+	//действия после авторизации
+});
 */
+
 
 /*
 //Получим: id, email, телефон, имя, временную зону пользователя
@@ -42,7 +49,62 @@ billManager.getUserBalance(function(data) {//Баланс пользовател
 });
 */
 
-//Получаем информацию по всем продуктам авторизованного пользователя с заданным кодом из биллинга (к примеру, soft)
-billManager.getUserProducts("soft", function(data) {//Продукты пользователя приходят сюда
+/*
+//Получаем информацию по всем продуктам авторизованного пользователя с заданным кодом из биллинга (к примеру, soft/vds/и т.п.)
+//Вторым параметром указывается массив доп. св-в, которые надо получить, по умолчанию возвращаются: id, наименование, цена, период, дата создания, дата истечения срока, статус услуги
+//Третьим параметром указывается массив с ID аддонов информацию по которым необходимо получить
+billManager.getUserProducts("vds", ["datacentername", "domain"], ["addon_13", "addon_15", "addon_9"], function(data) {//Продукты пользователя приходят сюда
 	console.log(data);
 });
+*/
+
+/*
+//Узнаем дату и сумму ближайшего платежа
+billManager.getNextPaymentInfo("vds", function(data) {//Сюда придет объект со свойствами date и price
+	console.log(data);
+});
+*/
+
+/*
+//Получает всю информацию по продуктам по их id в биллинге
+billManager.getProductsInfo([1,8], function(data) {//Сюда придет ответ
+	console.log(data);
+});
+*/
+
+/*
+//Смена пароля. Передаем id пользователя, старый пароль, новый пароль
+billManager.changePassword(13, "Alexander321", "Alexander123", function(data){
+	console.log(data);
+});
+*/
+
+/*
+//Пополнение баланса
+//Передаем сумму пополнение, валюту в iso, Id валюты в биллинге, id платежной системы в биллинге, callback
+//В callback придет URL на который надо перенаправить пользователя для осуществления платежа
+billManager.addFundsToBalance(1000, "RUB", 126, 8, function(data){
+	console.log(data);
+});
+*/
+
+
+/*
+//Покупка продукта
+//Первый аргумент объект, содержащий св-ва: product_code - код продукта из биллинга; product_id - id продукта из биллинга; period - период покупки; project - проект; addons - массив аддонов к продукту, если таковые есть, каждый элемент массива вида addon_X=Y, где X - Id аддона, Y - значения аддона;
+//amount - сумма покупки; currency_iso - iso валюты; currency_id - id валюты из биллинга; paymethod - id платежной системы из биллинг.
+//В callback придет URL на который надо перенаправить пользователя для оплаты заказа
+billManager.buyProduct({
+	product_code: "vds",
+	product_id: 8,
+	period: 1,
+	project: 1,
+	addons: ["addon_13=8192", "addon_15=10", "addon_9=260"],
+	amount: 196,
+	currency_iso: "USD",
+	currency_bm_id: 153,
+	paymethod: 8
+}, function(data){
+	console.log(data);
+});
+*/
